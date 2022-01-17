99daf60c49217fbe521fd69dba8b9ef79d3fc99f
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-01-17 18:07:20 -0300

    vqv-ok

--- src/average.js
+++ src/average.js
@@ -15,12 +15,12 @@
 const average = (array) => {
   if (array.length === 0) { 
     return undefined; 
-  };
+  }
   let sum = 0;
-  for(let index = 0; index < array.length; index += 1){
+  for (let index = 0; index < array.length; index += 1) {
     if (typeof array[index] !== 'number') { 
       return undefined; 
-    };
+    }
     sum += array[index];
   } 
   return Math.round(sum / array.length);
@@ -29,4 +29,3 @@ const average = (array) => {
 console.log((average([3, 4, 5])));
 
 module.exports = average;
-
