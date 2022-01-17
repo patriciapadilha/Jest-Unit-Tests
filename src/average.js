b6a5f7733ff7d73ed7525dfda12332978bd1de78
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-01-17 15:38:27 -0300

    average-ok

--- src/average.js
+++ src/average.js
@@ -12,9 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // add your implementation here
-
+const average = (array) => {
+  if (array.length === 0) { 
+    return undefined; 
+  };
+  let sum = 0;
+  for(let index = 0; index < array.length; index += 1){
+    if (typeof array[index] !== 'number') { 
+      return undefined; 
+    };
+    sum += array[index];
+  } 
+  return Math.round(sum / array.length);
 };
 
+console.log((average([3, 4, 5])));
+
 module.exports = average;
+
